e9e2447d2d76a052402c7c9e502320e70f91f212
Simon Maddox <simon@nas24.maddox>
2017-10-19 16:35:08 +0100

    Initial commit

---
+++ udmx.js
@@ -0,0 +1,28 @@
+module.exports = function(RED) {
+	var usb = require('usb');
+
+	function UDMXNode(config) {
+		RED.nodes.createNode(this, config);
+                this.status({text:'not found', shape:'ring', fill:'red'});
+		var udev = usb.findByIds(0x16c0, 0x5dc);
+		udev.open();
+		var node = this;
+		node.status({text:'connected', shape:'dot', fill:'green'});
+		node.on('input', function(msg) {
+
+			var channel = Number(msg.topic);
+			var level = Number(msg.payload);
+			var buf = Buffer(1);
+			udev.controlTransfer(64, 1, level, channel-1, buf, function(err, result) {
+				if (err) {
+					node.error(err);
+			                this.status({text:'error', shape:'ring', fill:'red'});
+				} else {
+					node.status({text:'ch'+('00'+channel).substr(-3)+':'+('00'+level).substr(-3), shape:'dot', fill:'green'});
+				}
+			});
+		});
+	}
+
+	RED.nodes.registerType("udmx", UDMXNode);
+};
